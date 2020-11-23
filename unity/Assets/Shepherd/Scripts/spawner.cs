489f3260d8ce09898a59726f658428e0588064e4
Yuri Maas <yurimaas@hotmail.com>
2020-11-23 17:32:58 +0100

    Update commit plus shepherd click

---
+++ unity/Assets/Shepherd/Scripts/spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    //public Transform spawnPos;
+    public GameObject shepherd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;
+            var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(shepherd, objectPos, Quaternion.identity);
+            //Instantiate(shepherd, spawnPos.position, spawnPos.rotation);
+        }
+    }
+}
