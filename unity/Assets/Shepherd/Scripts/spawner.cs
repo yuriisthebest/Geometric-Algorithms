b4e000142dca6119d06ba0ff224a64eaeb9eea82
Yuri Maas <yurimaas@hotmail.com>
2020-11-25 16:11:05 +0100

    Fixed spamming shepherds

--- unity/Assets/Shepherd/Scripts/spawner.cs
+++ unity/Assets/Shepherd/Scripts/spawner.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawner : MonoBehaviour
-{
+public class spawner : MonoBehaviour {
     //public Transform spawnPos;
     public GameObject shepherd;
+    private bool cooldown = false;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
+    void Update() {
+        // Create an object when the mouse is pressed, don't spam this function
+        if (Input.GetMouseButton(0) && !cooldown) {
             var mousePos = Input.mousePosition;
             mousePos.z = 2.0f;
             var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
             Instantiate(shepherd, objectPos, Quaternion.identity);
-            //Instantiate(shepherd, spawnPos.position, spawnPos.rotation);
+
+            Invoke("ResetCooldown", 0.5f);
+            cooldown = true;
         }
+    }
+
+    private void ResetCooldown() {
+        cooldown = false;
     }
 }
