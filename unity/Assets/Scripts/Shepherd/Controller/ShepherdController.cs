8ed5fa8821d7a720b3c30cef18a8667c18617dfd
Unknown <a.s.brouwers@student.tue.nl>
2020-12-22 13:54:29 +0100

    Implement drawing a DCEL

--- unity/Assets/Scripts/Shepherd/Controller/ShepherdController.cs
+++ unity/Assets/Scripts/Shepherd/Controller/ShepherdController.cs
@@ -12,6 +12,9 @@
     using Util.Geometry.DCEL;
     using Util.Geometry.Duality;
     using Util.Geometry.Polygon;
+    using Util.Geometry.Triangulation;
+    using Util.Algorithms.Triangulation;
+    using Voronoi;
 
     public class ShepherdController : MonoBehaviour, IController
     {
@@ -22,6 +25,9 @@
         [Header("Sheep Prefabs")]
         [SerializeField]
         private GameObject m_sheepPrefab;
+
+        [SerializeField]
+        private MeshFilter m_meshFilter;
 
         
         private int m_levelCounter = 0;
@@ -36,6 +42,21 @@
 
         public GameObject shepherd;
         private bool cooldown = false;
+
+        private enum EOwnership
+        {
+            UNOWNED,
+            PLAYER1,
+            PLAYER2,
+            PLAYER3,
+            PLAYER4
+        }
+
+        private readonly Dictionary<Face, EOwnership> m_ownership = new Dictionary<Face, EOwnership>();
+        [SerializeField]
+        private int playerIndex = 0;
+        
+        private DCEL m_dcel;
 
         void Start()
         {
@@ -53,6 +74,19 @@
                 // Don't let the user spam a lot of shepherds
                 Invoke("ResetCooldown", 0.5f);
                 cooldown = true;
+                
+                // Test: draw a square next to placed sheep
+                m_dcel = new DCEL();
+                var v1 = m_dcel.AddVertex(new Vector2(objectPos.x, objectPos.y));
+                var v2 = m_dcel.AddVertex(new Vector2(objectPos.x + 1, objectPos.y));
+                var v3 = m_dcel.AddVertex(new Vector2(objectPos.x + 1, objectPos.y + 1));
+                var v4 = m_dcel.AddVertex(new Vector2(objectPos.x, objectPos.y + 1));
+
+                m_dcel.AddEdge(v1, v2);
+                m_dcel.AddEdge(v2, v3);
+                m_dcel.AddEdge(v3, v4);
+                m_dcel.AddEdge(v4, v1);
+                UpdateMesh();
             }
         }
 
@@ -100,6 +134,7 @@
         public Polygon2D LocatePoint(Vector2D p, DCEL InGraph) 
         {
             var vc = VertDecomp(InGraph);
+            return null;
         }
 
         /* Create a vertical decomposition of the Voronoi diagram of the shepherds
@@ -119,11 +154,110 @@
         }
 
         // Anne
-        public void DrawVoronoi()
+        public void DrawVoronoi(DCEL VoronoiGraph)
         {
-        
+            
         }
 
+        /// <summary>
+        /// Updates the mesh according to the Voronoi DCEL.
+        /// (Edited from Voronoi game)
+        /// </summary>
+        private void UpdateMesh()
+        {
+            if (m_meshFilter.mesh == null)
+            {
+                // create initial mesh
+                m_meshFilter.mesh = new Mesh
+                {
+                    subMeshCount = 4
+                };
+                m_meshFilter.mesh.MarkDynamic();
+            }
+            else
+            {
+                // clear old mesh
+                m_meshFilter.mesh.Clear();
+                m_meshFilter.mesh.subMeshCount = 4;
+            }
+
+            // build vertices and triangle list
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>[4] {
+                new List<int>(),
+                new List<int>(),
+                new List<int>(),
+                new List<int>()
+            };
+
+            // iterate over vertices and create triangles accordingly
+            foreach (var face in m_dcel.Faces)
+            {
+                // Commented out part: TODO get color index from DCEL face
+
+                /*
+                // dont draw anything for unowned vertices
+                if (m_ownership[face] == EOwnership.UNOWNED) continue;
+
+                // get ownership of node
+                var playerIndex = -1;
+                switch (m_ownership[face]) {
+                    case EOwnership.PLAYER1:
+                        playerIndex = 0;
+                        break;
+                    case EOwnership.PLAYER2:
+                        playerIndex = 1;
+                        break;
+                    case EOwnership.PLAYER3:
+                        playerIndex = 2;
+                        break;
+                    case EOwnership.PLAYER4:
+                        playerIndex = 3;
+                        break;
+                }
+                */
+
+                playerIndex = 3;
+
+                // cant triangulate outer face
+                if (face.IsOuter) continue;
+
+                // triangulate face polygon
+                var triangulation = Triangulator.Triangulate(face.Polygon.Outside);
+
+                // add triangles to correct list
+                foreach (var triangle in triangulation.Triangles)
+                {
+                    int curCount = vertices.Count;
+
+                    // add triangle vertices
+                    vertices.Add(new Vector3(triangle.P0.x, 0, triangle.P0.y));
+                    vertices.Add(new Vector3(triangle.P1.x, 0, triangle.P1.y));
+                    vertices.Add(new Vector3(triangle.P2.x, 0, triangle.P2.y));
+
+                    // add triangle to mesh according to owner
+                    triangles[playerIndex].Add(curCount);
+                    triangles[playerIndex].Add(curCount + 1);
+                    triangles[playerIndex].Add(curCount + 2);
+                }
+            }
+
+            // update mesh
+            m_meshFilter.mesh.vertices = vertices.ToArray();
+            m_meshFilter.mesh.SetTriangles(triangles[0], 0);
+            m_meshFilter.mesh.SetTriangles(triangles[1], 1);
+            m_meshFilter.mesh.SetTriangles(triangles[2], 2);
+            m_meshFilter.mesh.SetTriangles(triangles[3], 3);
+            m_meshFilter.mesh.RecalculateBounds();
+
+            // set correct uv
+            var newUVs = new List<Vector2>();
+            foreach (var vertex in vertices)
+            {
+                newUVs.Add(new Vector2(vertex.x, vertex.z));
+            }
+            m_meshFilter.mesh.uv = newUVs.ToArray();
+        }
 
     }
 }
